41d42b935135d772dc53d74fe5884420cbc45f95
Aravind Hebbali <hebbali.aravind@gmail.com>
2020-04-23 11:50:02 +0000

    version 0.2.1

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,8 @@
   if (!interactive() || stats::runif(1) > 0.1) return()
 
   pkgs <- utils::available.packages()
-  
-  cran_version <- 
+
+  cran_version <-
     pkgs %>%
     magrittr::extract("rfm", "Version") %>%
     package_version()
@@ -22,10 +22,16 @@
 
   tip <- sample(tips, 1)
 
-  if (behind_cran) {
-    packageStartupMessage("A new version of rfm is available with bug fixes and new features.")
-  } else {
-    packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
-  }   
+  if (interactive()) {
+    if (behind_cran) {
+      msg <- c("A new version of rfm is available with bug fixes and new features.")
+      packageStartupMessage(msg, "\nWould you like to install it?")
+      if (utils::menu(c("Yes", "No")) == 1) {
+        utils::update.packages("rfm")
+      }
+    } else {
+      packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+    }
+  }
 
 }
--- inst/doc/rfm-transaction-level-data.R
+++ inst/doc/rfm-transaction-level-data.R
@@ -1,4 +1,4 @@
-## ---- echo=FALSE, message=FALSE------------------------------------------
+## ---- echo=FALSE, message=FALSE-----------------------------------------------
 library(rfm)
 library(knitr)
 library(kableExtra)
@@ -11,15 +11,15 @@ library(RColorBrewer)
 options(knitr.table.format = "html")
 options(tibble.width = Inf)
 
-## ----rfm_data_orders-----------------------------------------------------
+## ----rfm_data_orders----------------------------------------------------------
 rfm_data_orders
 
-## ----rfm_table_order, eval=FALSE-----------------------------------------
+## ----rfm_table_order, eval=FALSE----------------------------------------------
 #  analysis_date <- lubridate::as_date("2006-12-31", tz = "UTC")
 #  rfm_result <- rfm_table_order(rfm_data_orders, customer_id, order_date, revenue, analysis_date)
 #  rfm_result
 
-## ----rfm_table_order2, eval=TRUE, echo=FALSE-----------------------------
+## ----rfm_table_order2, eval=TRUE, echo=FALSE----------------------------------
 analysis_date <- lubridate::as_date("2006-12-31", tz = "UTC")
 rfm_result <- rfm_table_order(rfm_data_orders, customer_id, order_date, revenue, analysis_date)
 rfm_result %>%
@@ -28,28 +28,28 @@ rfm_result %>%
   kable() %>%
   kable_styling()
 
-## ----heatmap, fig.align='center', fig.width=8, fig.height=6--------------
+## ----heatmap, fig.align='center', fig.width=8, fig.height=6-------------------
 rfm_heatmap(rfm_result)
 
-## ----barchart, fig.align='center', fig.width=8, fig.height=6-------------
+## ----barchart, fig.align='center', fig.width=8, fig.height=6------------------
 rfm_bar_chart(rfm_result)
 
-## ----rfmhist, fig.align='center', fig.width=8, fig.height=6--------------
+## ----rfmhist, fig.align='center', fig.width=8, fig.height=6-------------------
 rfm_histograms(rfm_result)
 
-## ----rfmorders, fig.align='center', fig.width=8, fig.height=6------------
+## ----rfmorders, fig.align='center', fig.width=8, fig.height=6-----------------
 rfm_order_dist(rfm_result)
 
-## ----mr, fig.align='center', fig.width=7, fig.height=7-------------------
+## ----mr, fig.align='center', fig.width=7, fig.height=7------------------------
 rfm_rm_plot(rfm_result)
 
-## ----fm, fig.align='center', fig.width=7, fig.height=7-------------------
+## ----fm, fig.align='center', fig.width=7, fig.height=7------------------------
 rfm_fm_plot(rfm_result)
 
-## ----fr, fig.align='center', fig.width=7, fig.height=7-------------------
+## ----fr, fig.align='center', fig.width=7, fig.height=7------------------------
 rfm_rf_plot(rfm_result)
 
-## ----segments, echo=FALSE------------------------------------------------
+## ----segments, echo=FALSE-----------------------------------------------------
 segment <- c(
   "Champions", "Loyal Customers", "Potential Loyalist",
   "New Customers", "Promising", "Need Attention",
@@ -80,43 +80,23 @@ segments %>%
   kable() %>%
   kable_styling(full_width = TRUE, font_size = 12)
 
-## ----criteria, echo=FALSE------------------------------------------------
-rfm_segments <- rfm_result %>%
-  use_series(rfm) %>%
-  mutate(
-    segment = case_when(
-      (recency_score %>% between(4, 5)) & (frequency_score %>% between(4, 5)) &
-        (monetary_score %>% between(4, 5))  ~ "Champions",
-      (recency_score %>% between(2, 5)) & (frequency_score %>% between(3, 5)) &
-        (monetary_score %>% between(3, 5)) ~ "Loyal Customers",
-      (recency_score %>% between(3, 5)) & (frequency_score %>% between(1, 3)) &
-        (monetary_score %>% between(1, 3)) ~ "Potential Loyalist",
-      (recency_score %>% between(4, 5)) & (frequency_score == 1) &
-        (monetary_score == 1) ~ "New Customers",
-      (recency_score %>% between(3, 4)) & (frequency_score == 1) &
-        (monetary_score == 1) ~ "Promising",
-      (recency_score %>% between(2, 3)) & (frequency_score %>% between(2, 3)) &
-        (monetary_score %>% between(2, 3)) ~ "Needs Attention",
-      (recency_score %>% between(2, 3)) & (frequency_score <= 2) &
-        (monetary_score <= 2) ~ "About To Sleep",
-      (recency_score <= 2) & (frequency_score %>% between(2, 5)) &
-        (monetary_score %>% between(2, 5)) ~ "At Risk",
-      (recency_score == 1) & (frequency_score %>% between(4, 5)) &
-        (monetary_score %>% between(4, 5)) ~ "Cant Lose Them",
-      (recency_score %>% between(1, 2)) & (frequency_score %>% between(1, 2)) &
-        (monetary_score %>% between(1, 2)) ~ "Hibernating",
-      (recency_score <= 2) & (frequency_score <= 2) &
-        (monetary_score <= 2) ~ "Lost",
-      TRUE ~ "Others"
-    )
-  ) %>%
-  select(
-    customer_id, segment, rfm_score, transaction_count, recency_days,
-    amount
-  )
+## ----criteria, echo=FALSE-----------------------------------------------------
+segment_names <- c("Champions", "Loyal Customers", "Potential Loyalist",
+  "New Customers", "Promising", "Need Attention", "About To Sleep",
+  "At Risk", "Can't Lose Them", "Lost")
+
+recency_lower <- c(4, 2, 3, 4, 3, 2, 2, 1, 1, 1)
+recency_upper <- c(5, 5, 5, 5, 4, 3, 3, 2, 1, 2)
+frequency_lower <- c(4, 3, 1, 1, 1, 2, 1, 2, 4, 1)
+frequency_upper <- c(5, 5, 3, 1, 1, 3, 2, 5, 5, 2)
+monetary_lower <- c(4, 3, 1, 1, 1, 2, 1, 2, 4, 1)
+monetary_upper <- c(5, 5, 3, 1, 1, 3, 2, 5, 5, 2)
+
+segments <- rfm_segment(rfm_result, segment_names, recency_lower, recency_upper,
+frequency_lower, frequency_upper, monetary_lower, monetary_upper)
 
 # use datatable
-rfm_segments %>%
+segments %>%
   datatable(
     filter = "top",
     options = list(pageLength = 5, autoWidth = TRUE),
@@ -126,69 +106,18 @@ rfm_segments %>%
     )
   )
 
-## ----rfm_customers-------------------------------------------------------
-rfm_segments %>%
+## ----rfm_customers------------------------------------------------------------
+segments %>%
   count(segment) %>%
   arrange(desc(n)) %>%
   rename(Segment = segment, Count = n)
 
-## ----avg_recency, fig.align='center', fig.height=5, fig.width=6----------
-data <- 
-    rfm_segments %>%
-    group_by(segment) %>%
-    select(segment, recency_days) %>%
-    summarize(median(recency_days)) %>%
-    rename(segment = segment, avg_recency = `median(recency_days)`) %>%
-    arrange(avg_recency) 
+## ----avg_recency, fig.align='center', fig.height=5, fig.width=6---------------
+rfm_plot_median_recency(segments)
 
-  n_fill <- nrow(data)
+## ----avg_frequency, fig.align='center', fig.height=5, fig.width=6-------------
+rfm_plot_median_frequency(segments)
 
-  ggplot(data, aes(segment, avg_recency)) +
-    geom_bar(stat = "identity", fill = brewer.pal(n = n_fill, name = "Set1")) +
-    xlab("Segment") + ylab("Median Recency") +
-    ggtitle("Median Recency by Segment") +
-    coord_flip() +
-    theme(
-      plot.title = element_text(hjust = 0.5)
-    )
-
-## ----avg_frequency, fig.align='center', fig.height=5, fig.width=6--------
-data <- 
-    rfm_segments %>%
-    group_by(segment) %>%
-    select(segment, transaction_count) %>%
-    summarize(median(transaction_count)) %>%
-    rename(segment = segment, avg_frequency = `median(transaction_count)`) %>%
-    arrange(avg_frequency) 
-
-n_fill <- nrow(data)
-
-ggplot(data, aes(segment, avg_frequency)) +
-    geom_bar(stat = "identity", fill = brewer.pal(n = n_fill, name = "Set1")) +
-    xlab("Segment") + ylab("Median Frequency") +
-    ggtitle("Median Frequency by Segment") +
-    coord_flip() +
-    theme(
-      plot.title = element_text(hjust = 0.5)
-    )
-
-## ----avg_monetary, fig.align='center', fig.height=5, fig.width=6---------
-data <- 
-    rfm_segments %>%
-    group_by(segment) %>%
-    select(segment, amount) %>%
-    summarize(median(amount)) %>%
-    rename(segment = segment, avg_monetary = `median(amount)`) %>%
-    arrange(avg_monetary) 
-
-n_fill <- nrow(data)
-
-ggplot(data, aes(segment, avg_monetary)) +
-    geom_bar(stat = "identity", fill = brewer.pal(n = n_fill, name = "Set1")) +
-    xlab("Segment") + ylab("Median Monetary Value") +
-    ggtitle("Median Monetary Value by Segment") +
-    coord_flip() +
-    theme(
-      plot.title = element_text(hjust = 0.5)
-    )
+## ----avg_monetary, fig.align='center', fig.height=5, fig.width=6--------------
+rfm_plot_median_monetary(segments)
 
